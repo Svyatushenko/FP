d5e98ddf9be5fa3b0ccfc7f92f81ade855531553
Svyatushenko Elena <svyatushenko@gmail.com>
2012-11-09 03:17:46 +0400

    another Categories

---
+++ ITMOPrelude/Categories/FromMonadFish.hs
@@ -0,0 +1,19 @@
+﻿{-# LANGUAGE NoImplicitPrelude, FlexibleInstances, UndecidableInstances #-}
+module ITMOPrelude.Categories.ToMonadFish where
+import ITMOPrelude.Categories.MonadFish
+
+-- Эти
+import ITMOPrelude.Categories
+import ITMOPrelude.Categories.MonadJoin
+
+-- делаем из нас
+instance MonadFish m => Monad m where
+    return 	= returnFish
+    ma >>= f 	= (id >=> f) ma
+
+instance MonadFish m => Functor m where
+    fmap f ma 	= (id >=> (returnFish . f)) ma
+
+instance MonadFish m => MonadJoin m where
+    returnJoin 	= returnFish
+    join 	= id >=> id
